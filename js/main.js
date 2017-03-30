b662f711fae4ba3f4534f23f7e817844cc624205
David Avellaneda <g.avella@gmail.com>
2017-03-30 13:47:06 -0500

    Agregando script para ver toda la interacción

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
 console.log('Hola Mundo <3')
+
+var $form = $('#contact-form')
+
+$('#biography-btn').on('click', function () {
+  $('#biography-cont').toggleClass('hidden')
+})
+
+$('#contact-btn').on('click', function () {
+  $('#contact-form').toggleClass('hidden')
+})
+
+$form.on('submit', function (e) {
+  e.preventDefault()
+  $.post($form.attr('action'), {
+      email: $('#email').val(),
+      text: $('#text').val()
+    })
+    .then(function (res) {
+      alert(res.message)
+    }, function (err) {
+      console.error(err)
+      alert('Error sending the form.')
+    })
+})
